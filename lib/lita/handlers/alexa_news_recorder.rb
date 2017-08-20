86239aad99bff7512860f189ffffccddca7edce7
Daniel J. Pritchett <dpritchett@gmail.com>
2017-08-20 15:52:30 -0500

    Dump amazon post into debug log so we can figure out how to handle it

--- lib/lita/handlers/alexa_news_recorder.rb
+++ lib/lita/handlers/alexa_news_recorder.rb
@@ -1,7 +1,18 @@
+require 'pry'
+
 module Lita
   module Handlers
     class AlexaNewsRecorder < Handler
-      # insert handler code here
+      http.post '/alexa/recorder', :record_message
+
+      def record_message(request, response)
+        Lita.logger.debug "Inbound request params: "
+        Lita.logger.debug request.params
+        Lita.logger.debug "Inbound request body: "
+        Lita.logger.debug request.body
+
+        response.write request.body
+      end
 
       Lita.register_handler(self)
     end
