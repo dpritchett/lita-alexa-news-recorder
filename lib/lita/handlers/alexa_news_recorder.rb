70305adddc69d4d46f4cb04fdeacba5c6c320cef
Daniel J. Pritchett <dpritchett@gmail.com>
2017-08-27 16:48:26 -0500

    Tested request/response

--- lib/lita/handlers/alexa_news_recorder.rb
+++ lib/lita/handlers/alexa_news_recorder.rb
@@ -6,25 +6,13 @@ module Lita
     class AlexaNewsRecorder < Handler
       http.post '/alexa/recorder', :record_message
 
+      # Rack::Request, Rack::Response
       def record_message(request, response)
-        check_for_publisher!
-
-        Lita.logger.debug "Inbound request params: "
-        Lita.logger.debug request.params
-        Lita.logger.debug "Inbound request body: "
-
-        Lita.logger.debug request.body.string # it's a StringIO
-
         message = extract_message(request.body.string)
 
         robot.trigger(:save_alexa_message, username: 'Alexa News Recorder', message: message)
 
         response.write JSON.dump(alexa_response(message))
-      end
-
-      def check_for_publisher!
-        return if robot.handlers.include?(Lita::Handlers::AlexaNewsPublisher)
-        raise ConfigurationError, "Must install AlexaNewsPublisher handler to make me work!"
       end
 
       def extract_message(payload)
--- spec/lita/handlers/alexa_news_recorder_spec.rb
+++ spec/lita/handlers/alexa_news_recorder_spec.rb
@@ -1,4 +1,42 @@
 require "spec_helper"
+require 'pry'
+require 'date'
 
 describe Lita::Handlers::AlexaNewsRecorder, lita_handler: true do
+  let(:robot) { Lita::Robot.new(registry) }
+
+  subject { described_class.new(robot) }
+
+  describe 'routes' do
+    it { is_expected.to route_http(:post, "/alexa/recorder").to(:record_message) }
+  end
+
+  describe ':record_message' do
+    # "hello, Alexa" fixture
+    let(:amazon_formatted_json_request_body) { open("./spec/fixtures/inbound_message.json").read }
+    
+    it 'can accept an inbound message in alexa dictation format' do
+      response = http.post('/alexa/recorder', amazon_formatted_json_request_body)
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(body.dig(:response, :outputSpeech, :text)).to include('hi dad')
+    end
+
+    it 'emits an event to trigger another handler to save alexa messages' do
+      expect(robot).to receive(:trigger).with(:save_alexa_message, {:username=>"Alexa News Recorder", :message=>"hi dad"})
+
+      response = http.post('/alexa/recorder', amazon_formatted_json_request_body)
+    end
+  end
+
+  describe ':alexa_response' do
+    let(:body) { 'responding to alexa' }
+
+    it 'should return an alexa-shaped response hash with the supplied message body' do
+      result = subject.alexa_response(body)
+
+      expect(result.dig(:response, :outputSpeech, :text)).to include(body)
+    end
+  end
+
 end
